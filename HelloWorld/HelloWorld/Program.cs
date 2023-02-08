7833a186a931e1e7d4c9dc9b4c382203f63804dc
rgay <rgay@bitwerx.com>
2023-02-08 13:14:03 -0500

    chapters 1-7

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,256 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello! I am in a C# RPG game"); // string literal
+Console.WriteLine("And I need a few statements"); // string literal
+Console.WriteLine("2 more after this"); // string literal
+Console.WriteLine("one more..."); // string literal
+Console.WriteLine("done!"); // string literal*/
+// . = member access operator
+// code map: Base Class Library (BCL) -> system (namespace) -> Console (class) -> WriteLine (method)
+// BCL also includes things like Convert and Math classes
+/*
+Console.WriteLine("Bread is ready.\nWho is the bread ready for?");
+string name = Console.ReadLine() ?? "default"; // compiler warning (could be an issue) vs a compiler error
+Console.WriteLine($"Notes: {name} got bread.");*/
+// build configs -> release has code optimization turns on that will do things like remove unused variables. Good for releases but can make debugging harder
+
+/*Console.WriteLine("What kind of thing are we talking about?"); 
+string item = Console.ReadLine() ?? ""; // item
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?"); 
+string adj = Console.ReadLine() ?? ""; *//* adjective *//*
+string ofDoom = "of Doom"; 
+string version = "3000"; 
+Console.WriteLine($"The {adj} {item} of {ofDoom} {version}!");
+
+// muli value initilization and assignment
+int a, b, c = 10;
+a = b = c = 7;
+Console.WriteLine(a + b + c);
+// quiz: name, type value / true / no / answer, value1, delete_me, PI
+
+// built in / primitive types
+byte w; //      0 - 256 (size: 1 byte)
+sbyte ww; // -128 - 127
+
+short x; //  -32k~ - 32k~
+int y; // -2 billion~ - 2 billion (4 bytes)
+long z; // -large number - large number (8 bytes)
+ushort xx; // 0 - 65535 (2 bytes)
+uint yy = 0xFF4; // hex literal 
+ulong zz = 0b1011; // binary literal
+
+char yeah = '\u0061'; // 2 bytes? ascii or unicode -> \uxxxx = unicode
+
+byte test = 257 - 2;
+
+
+double sci = 43e22; // scientifici notation
+
+// float, doube, decimal (money)
+// computer will convert anything smaller than an int to int to do math and convert it back so the smaller values aren't really that useful*/
+
+// each of 14 variable types
+/*byte b = 255;
+sbyte sb = -12;
+short s = 32_000;
+ushort us = 65000;
+int i = -2_000_000;
+uint ui = 4_000_000;
+long l = 0xFFFFFFFF;
+ulong ul = 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000001;
+
+char c = '\u0123';
+string str = "hi!!";
+
+float f = 4.0f;
+double d = 84.3e23;
+decimal dec = 44.42m;
+
+bool isAllOfThem = true;
+
+// quiz: false / byte, short, int, long / false / uint / float, double (4.0f), decimal (4.0m) / double (decimal trades percision for size, i think) / decimal / string / boolean (bool)
+
+int[] marks = new int[] {23, 45, 67, 88, 99,
+                            56, 27, 67, 89, 90, 39};
+
+// range struct:
+var a3 = marks[2..4];
+Console.Write("\nMarks List 3: ");
+foreach (var st_3 in a3)
+    Console.Write($" {st_3} ");
+Console.Write("\n");
+
+// use checked and unchecked to either check for (and throw an error) if there is overflow, or to not check (unchecked)
+uint a = uint.MaxValue;
+
+unchecked
+{
+    Console.WriteLine(a + 1);  // output: 0
+}
+
+try
+{
+    checked
+    {
+        Console.WriteLine(a + 1);
+    }
+}
+catch (OverflowException e)
+{
+    Console.WriteLine(e.Message);  // output: Arithmetic operation resulted in an overflow.
+}
+
+
+// cool reference material:
+// https://csharpplayersguide.com/articles/operators-table
+
+unsafe // added to .csproj: <AllowUnsafeBlocks>true</AllowUnsafeBlocks>
+{
+    int test = 4;
+    int *address = &test;
+    Console.WriteLine($"Address cast to int: {(int)address}");
+    Console.WriteLine($"Address via *: {*address}");
+}
+
+// Compute area of triangle
+var keepRunning = true;
+while(keepRunning)
+{
+    Console.WriteLine("Calculate the area of a triange...");
+    Console.WriteLine("Enter a base length:");
+    double baseValue;
+    double.TryParse(Console.ReadLine(), out baseValue);
+    double height;
+    Console.WriteLine("Enter a height:");
+    double.TryParse(Console.ReadLine(), out height);
+    double area = baseValue * height / 2.0;
+    Console.WriteLine($"{area} = {baseValue} * {height} / 2");
+    Console.WriteLine("Enter 'y' to run again...");
+    var runAgain = Console.ReadLine();
+    if (runAgain != "y") keepRunning = false;
+}
+
+int testInt = short.MinValue;
+Console.WriteLine(testInt);
+Console.WriteLine(double.NegativeInfinity);
+Console.WriteLine(double.NaN);
+Console.WriteLine(8.0 / 0); // floating point division by zero assumes that you are actually dividing by an incredibly small (infinitesmal) but non-zero number
+Console.WriteLine(-8.0 / 0);
+Console.WriteLine(0.0 / 0); // 0.0 / 0.0 = NaN
+
+int aaa = -1;
+Console.WriteLine(+aaa);
+aaa = -aaa;
+Console.WriteLine(aaa);
+aaa = -aaa;
+Console.WriteLine(aaa);*/
+
+/*var keepRunning = true;
+while (keepRunning)
+{
+    Console.WriteLine("Enter the number of chocolate eggs laid today:");
+    int eggs;
+    int.TryParse(Console.ReadLine(), out eggs);
+    Console.WriteLine($"Total eggs: {eggs}");
+    Console.WriteLine($"Each of the 4 sisters will recieve {eggs / 4} eggs");
+    Console.WriteLine($"Duckbear will recieve {eggs % 4} eggs");
+    Console.WriteLine("Enter 'y' to run again...");
+    var runAgain = Console.ReadLine();
+    if (runAgain != "y") keepRunning = false;
+}*/
+
+// Melik, Casik, and Balik
+
+/*using System.Threading.Tasks;
+
+var keepRunning = true;
+var kings = new string[3] { "Melik", "Casik", "Balik" };
+while (keepRunning)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine($"Enter the number of estates for {kings[i]}:");
+        int estates;
+        int.TryParse(Console.ReadLine(), out estates);
+        Console.WriteLine($"Enter the number of duchyes for {kings[i]}:");
+        int duchyes;
+        int.TryParse(Console.ReadLine(), out duchyes);
+        Console.WriteLine($"Enter the number of provinces for {kings[i]}:");
+        int provinces;
+        int.TryParse(Console.ReadLine(), out provinces);
+        Console.WriteLine($"{kings[i]} has the following points:");
+        Console.WriteLine($"estates: {estates} x 1 point = {estates *= 1}");
+        Console.WriteLine($"duchyes: {duchyes} x 3 points = {duchyes *= 3}");
+        Console.WriteLine($"provinces: {provinces} x 6 points = {provinces *= 6}");
+        Console.WriteLine($"Total = {estates + duchyes + provinces}");
+    }
+
+    Console.WriteLine("Enter 'y' to run again...");
+    var runAgain = Console.ReadLine();
+    if (runAgain != "y") keepRunning = false;
+}*/
+
+/*byte a = byte.MaxValue;
+byte b = 4;
+byte result = (byte)(a + b); // no addition defined for shorts so there is implicit casting to int, so you have to do an explicit cast back to int
+Console.WriteLine(result);
+
+Console.WriteLine(Math.Clamp(50, 0, 100)); // keeps a value in a range*/
+
+/*Console.Title = "A new adventure begins...";
+Console.Write("What is your name, human? ");
+var name = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@$"Welcome to the game /\/\ {name} /\/\");*/
+
+/*for (int i = 0; i < 1000; i++)
+{
+    Console.ForegroundColor = (ConsoleColor)(i % 16);
+    Console.BackgroundColor = (ConsoleColor)(i % 16);
+    Console.WriteLine();
+    Thread.Sleep(100);
+}*/
+
+
+// you could create a graphic display program that pints color line by line
+// Create some rainbows
+for (int i = 0; i < 100; i++)
+{
+    var colorNumber = i % 16;
+    Console.Title = $"Adding color: {Enum.GetName(typeof(ConsoleColor), colorNumber)}";
+    Console.BackgroundColor = (ConsoleColor)(colorNumber);
+    Console.Write("                   ");
+    Thread.Sleep(50);
+}
+
+// make some music
+Thread.Sleep(500);
+Console.WriteLine();
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Title = "Playing F\u00FCr Elise...";
+
+double bpm = 30;
+double quarter = (60 / bpm) * 1000;
+var eighth = quarter / 2;
+var sixteenth = quarter / 4;
+var half = quarter * 2;
+var whole = quarter * 4;
+
+var notes = new List<(int, double)>() {
+    (659, sixteenth), // E5
+    (622, sixteenth), // D#5
+    (659, sixteenth), // E5
+    (622, sixteenth), // D#5
+    (659, sixteenth), // E5
+    (493, sixteenth), // B4
+    (587, sixteenth), // D5
+    (523, sixteenth), // C5
+    (440, eighth) // A4
+};
+for (int i = 0; i < notes.Count; i++)
+{
+    var note = notes[i];
+    Console.WriteLine($"freq: {note.Item1} -- duration: {note.Item2}");
+    Console.Beep(note.Item1, (int)note.Item2);
+}
+
+Console.ReadKey(true);
