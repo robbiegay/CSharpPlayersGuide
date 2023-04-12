c0f9cc1427a29220346747c07e80995bf435ab3a
rgay <rgay@bitwerx.com>
2023-04-12 09:46:41 -0400

    Finished Level 25

--- HelloWorld/HelloWorld/Levels/Level25.cs
+++ HelloWorld/HelloWorld/Levels/Level25.cs
@@ -120,15 +120,229 @@ if (obj is string myString2)
             Utilities.TypewriterType("This is a message", 100, 2000, true, false);
             Utilities.TypewriterType("And I am writing this next", 100, 2000, false, true);
             Console.WriteLine("----------------------------------");
-
-            // TODO: Review notes, maybe research hash codes
         }
 
         public static void PackingInventory()
         {
+            Console.WriteLine("Create a new pack!\n\nEnter a max item count (int):");
+            int maxCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a max weight count (double):");
+            double maxWeight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a max volume count (double):");
+            double maxVolume = double.Parse(Console.ReadLine());
 
+            var pack = new Pack(maxCount, maxWeight, maxVolume);
+
+            var keepRunning = true;
+
+            while (keepRunning) 
+            {
+                pack.Print();
+
+                Console.WriteLine("Select an item to add to your pack:");
+                Console.WriteLine("\t\te - exit");
+                Console.WriteLine("\t\t1 - arrow");
+                Console.WriteLine("\t\t2 - bow");
+                Console.WriteLine("\t\t3 - rope");
+                Console.WriteLine("\t\t4 - water");
+                Console.WriteLine("\t\t5 - food");
+                Console.WriteLine("\t\t6 - sword");
+
+                var input = Console.ReadKey(true).KeyChar;
+
+                if (input == 'e')
+                {
+                    keepRunning = false;
+                }
+
+                InventoryItem itemToAdd = input switch
+                {
+                    '1' => new Arrow(),
+                    '2' => new Bow(),
+                    '3' => new Rope(),
+                    '4' => new Water(),
+                    '5' => new Food(),
+                    '6' => new Sword(),
+                    _ => new Arrow() // default case
+                };
+
+                pack.AddItem(itemToAdd);
+            }
         }
 
-        
+        private class Pack
+        {
+            public List<InventoryItem> Items { get; set; }
+            public int MaxItemCount { get; init; }
+            public double MaxWeight { get; init; }
+            public double MaxVolume { get; init; }
+            public int ItemCount 
+            {
+                get => Items.Count;
+            }
+            public double CurrentWeight 
+            { 
+                get
+                {
+                    var total = 0.0;
+
+                    foreach (var item in Items)
+                    {
+                        total += item.Weight;
+                    }
+
+                    return total;
+                }
+            }
+
+            public double CurrentVolume
+            {
+                get
+                {
+                    var total = 0.0;
+
+                    foreach (var item in Items)
+                    {
+                        total += item.Volume;
+                    }
+
+                    return total;
+                }
+            }
+
+            public Pack(int maxCount, double maxWeight, double maxVolume)
+            {
+                Items = new List<InventoryItem>();
+
+                MaxItemCount = maxCount;
+                MaxWeight = maxWeight;
+                MaxVolume = maxVolume;
+            }
+
+            public bool AddItem(InventoryItem item)
+            {
+                var result = true;
+
+                if (CurrentWeight + item.Weight > MaxWeight)
+                {
+                    Utilities.PrintInColor("Item weighs too much...", 1);
+                    result = false;
+                }
+                if (CurrentVolume + item.Volume > MaxVolume)
+                {
+                    Utilities.PrintInColor("Item has too much volume...", 1);
+                    result= false;
+                }
+                if (ItemCount + 1 > MaxItemCount)
+                {
+                    Utilities.PrintInColor("Item count is too high...", 1);
+                    result = false;
+                }
+
+                if (result == true)
+                {
+                    Items.Add(item);
+                    Utilities.PrintInColor($"{nameof(item)} added!", 7);
+                }
+
+                return result;
+            }
+
+            public void Print()
+            {
+                Utilities.PrintInColor("\n---------------------------------------------\n", 14);
+                Utilities.PrintInColor("Your pack contains the following:", 3);
+
+                Utilities.PrintInColor("\n---------------------------------------------\n", 9);
+                if (this.ItemCount == 0)
+                    Utilities.PrintInColor("[Empty]", 5);
+
+                foreach (var item in Items)
+                {
+                    Utilities.PrintInColor($"\t- {item.GetType().Name}: {item.Weight} weight, {item.Volume} volume", 2);
+                }
+                Utilities.PrintInColor("\n---------------------------------------------\n", 9);
+
+                Utilities.PrintInColor($"Total pack count: ", 2, true);
+                Utilities.PrintInColor($"{this.ItemCount}", 7, true);
+                Utilities.PrintInColor($" / ", 2, true);
+                Utilities.PrintInColor($"{this.MaxItemCount}", 1, true);
+                Console.WriteLine("");
+
+                Utilities.PrintInColor($"Total pack weight: ", 2, true);
+                Utilities.PrintInColor($"{this.CurrentWeight}", 7, true);
+                Utilities.PrintInColor($" / ", 2, true);
+                Utilities.PrintInColor($"{this.MaxWeight}", 1, true);
+                Console.WriteLine("");
+
+                Utilities.PrintInColor($"Total pack volume: ", 2, true);
+                Utilities.PrintInColor($"{this.CurrentVolume}", 7, true);
+                Utilities.PrintInColor($" / ", 2, true);
+                Utilities.PrintInColor($"{this.MaxVolume}", 1, true);
+                Console.WriteLine("");
+
+                Utilities.PrintInColor("\n---------------------------------------------\n", 14);
+            }
+        }
+
+        private class Arrow : InventoryItem
+        {
+            public Arrow() : base(0.1, 0.05)
+            {
+                //
+            }
+        }
+
+        private class Bow : InventoryItem
+        {
+            public Bow() : base(1, 4)
+            {
+                //
+            }
+        }
+
+        private class Rope : InventoryItem
+        {
+            public Rope() : base(1, 1.5)
+            {
+                //
+            }
+        }
+
+        private class Water : InventoryItem
+        {
+            public Water() : base(2, 3)
+            {
+                //
+            }
+        }
+
+        private class Food : InventoryItem
+        {
+            public Food() : base(1, 0.5)
+            {
+                //
+            }
+        }
+
+        private class Sword : InventoryItem
+        {
+            public Sword() : base(5, 3)
+            {
+                //
+            }
+        }
+
+        private class InventoryItem
+        {
+            public double Weight { get; init; }
+            public double Volume { get; init; }
+
+            public InventoryItem(double weight, double volume)
+            {
+                Weight = weight;
+                Volume = volume;
+            }
+        }
     }
 }
